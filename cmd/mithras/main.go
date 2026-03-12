debf66175f2ab7ec2d48721ac31def1150db84ce
Stefan Rizescu <iulian.stefan.rizescu@gmail.com>
2026-03-12 22:29:49 +0200

    feat: add initial Go CLI project structure

---
+++ cmd/mithras/main.go
@@ -0,0 +1,23 @@
+// Package main provides the mithras CLI application.
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/urfave/cli/v3"
+)
+
+func main() {
+	cmd := &cli.Command{
+		Name:     "mithras",
+		Usage:    "TODO",
+		Commands: []*cli.Command{},
+	}
+
+	if err := cmd.Run(context.Background(), os.Args); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/rizesql/mithras
 
 go 1.26.1
+
+require github.com/urfave/cli/v3 v3.7.0
